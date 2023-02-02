0c9deb77256c5735c769526bd6c330f831f1417c
“Chulin <chen6496@umn.edu>
2023-02-01 20:43:35 -0600

    Data Import and Cleaning

--- week3.R
+++ week3.R
@@ -1,1 +1,9 @@
+# Script Settings and Resources
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
+#Data Import and Cleaning
+raw_df <- read.csv("data/week3.csv")
+raw_df$timeStart <- as.POSIXct(raw_df$timeStart)
+raw_df$timeEnd <- as.POSIXct(raw_df$timeEnd)
+clean_df <- raw_df[format(raw_df$timeStart, "%m") != "06",]
+clean_df <- clean_df[clean_df$q6 == 1,]
