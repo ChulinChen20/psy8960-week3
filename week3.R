7886a66a66809d72b51e23cfe91d64fe2ba2a8a5
[chulinchen20] <[chen6496@umn.edu]>
2024-01-29 04:55:40 -0600

    Update

--- week3.R
+++ week3.R
@@ -2,19 +2,20 @@
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 # Data Import and Cleaning
-raw_df <- read.csv("data/week3.csv")
+raw_df <- read.csv("../data/week3.csv") 
 raw_df$timeStart <- as.POSIXct(raw_df$timeStart)
 raw_df$timeEnd <- as.POSIXct(raw_df$timeEnd)
-clean_df <- raw_df[format(raw_df$timeStart, "%m") != "06",]
+clean_df <- raw_df[raw_df$timeStart > "2017-07-01 00:00:00",] # raw_df[!grepl("2017-06", raw_df$timeStart),]
 clean_df <- clean_df[clean_df$q6 == 1,]
 
 # Analysis
-clean_df$timeSpent <- difftime(clean_df[,2],clean_df[,1], units = "secs")
+clean_df$timeSpent <- difftime(clean_df$timeEnd, clean_df$timeStart,units = "secs") # clean_df$timeSpent <- as.numeric(clean_df$timeEnd - clean_df$timeStart) * 60
 hist(as.numeric(clean_df$timeSpent))
 frequency_tables_list <- lapply(clean_df[,5:14], table)
 lapply(frequency_tables_list,barplot)
-nrow(clean_df[clean_df[,5]>=clean_df[,6] & clean_df[,6] != clean_df[,7],])
+sum(clean_df$q1 >= clean_df$q2 & clean_df$q2 != clean_df$q3)
 for (i in frequency_tables_list) {
   barplot(i) }
+
 
 
